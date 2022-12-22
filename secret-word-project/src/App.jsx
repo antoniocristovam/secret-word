2330232fec1b42312ac7261c55561a9b35fdd3d5
Antonio Cristovam <84741807+antoniocristovam@users.noreply.github.com>
2022-12-21 22:10:40 -0300

    Adicionando rotas

--- secret-word-project/src/App.jsx
+++ secret-word-project/src/App.jsx
@@ -21,8 +21,19 @@ const stages = [
 ];
 
 function App() {
+  //Stars the secret word game
   const startGame = () => {
     setGameStage(stages[1].name);
+  };
+
+  // process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  //Restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
   };
 
   const [gameStage, setGameStage] = useState(stages[0].name);
@@ -34,8 +45,8 @@ function App() {
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame} />}
-      {gameStage === "game" && <Game />}
-      {gameStage === "end" && <GameOver />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
