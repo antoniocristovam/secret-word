44755082fb60aa7bcb3f525e306456f54c7c9ce5
Antonio Cristovam <84741807+antoniocristovam@users.noreply.github.com>
2022-12-25 00:01:27 -0300

    Adicionando logo do projeto

--- secret-word-project/src/components/StartScreen.jsx
+++ secret-word-project/src/components/StartScreen.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./StartScreen.css";
+import logo_sw from "./sw.png";
 
 const StartScreen = ({ startGame, userName }) => {
   return (
     <div className="start">
-      <h1>🎮 Secret Word 🎮</h1>
+      <img className="logo" width={450} src={logo_sw} alt="Logo sw" />
+      {/* <h1>🎮 Secret Word 🎮</h1> */}
       <h2>Olá {userName}, seja bem vindo (a)</h2>
       <p>Clique no botão a baixo para iniciar o jogo.</p>
       <button onClick={startGame}>Começar o jogo</button>
